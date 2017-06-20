ed653c19d376730d29519575f2706f0bf63cfdbc
Brandon Bakker <brandon@bdmd.com.au>
2017-06-21 08:04:31 +1000

    Adding more complex resolving logic for node_modules components

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const Promise = require('bluebird');
 const readFile = Promise.promisify(fs.readFile);
 const resolveDependancy = require('resolve');
+const findNodeModules = require('find-node-modules');
 
 function getDependanciesFromFile(fileName) {
   return new Promise((resolve, reject) => {
@@ -22,6 +23,7 @@ function getDependanciesFromFile(fileName) {
             'decorators',
             'classProperties',
             'classPrivateProperties',
+            'objectRestSpread'
           ],
         }
       );
@@ -38,12 +40,20 @@ function getDependanciesFromFile(fileName) {
 
 function resolveDependancyToFile(dependancyName) {
   return new Promise((resolve, reject) => {
-    const curriedResolve = R.curry(resolveDependancy);
-    const c = curriedResolve(dependancyName, { basedir: __dirname });
-    const promisedC = Promise.promisify(c);
-    promisedC().then((val) => {
-      resolve(val);
-    }).catch(reject);
+    try {
+      console.log(findNodeModules());
+      resolveDependancy(dependancyName, { basedir: findNodeModules()[0] + '/' + dependancyName }, (val) => {
+        resolve(val);
+      });
+    } catch(e) {
+      reject(e);
+    }
+    // const curriedResolve = R.curry(resolveDependancy);
+    // const c = curriedResolve(dependancyName, { basedir: process.cwd() });
+    // const promisedC = Promise.promisify(c);
+    // promisedC().then((val) => {
+    // resolve(val);
+    // }).catch(reject);
   });
 }
 
@@ -59,14 +69,11 @@ function safeAwait(promise, errorMessage) {
 
 async function start() {
   const file = process.argv[2];
-  const dependancies = await safeAwait(
-    getDependanciesFromFile(file), 
-    `Could not get file. You supplied the path: ${file}. Check to make sure it exists in the current path and is accessible`
-  );
+  const dependancies = await getDependanciesFromFile(file);
 
-  Promise.all(resolveMapDependanciesToFile(dependancies).then(paths => {
+  Promise.all(resolveMapDependanciesToFile(dependancies)).then(paths => {
     console.log(paths);
-  })).catch(console.error(
+  }).catch(console.error(
     'Dependancy was not found in current project space. \
     Are you sure you called Sideswipe from the project directory'
   ));
