4e120fc83ebf4af4b71a55d9fdca2d429008dbd0
Brandon Bakker <brandon@bdmd.com.au>
2017-07-12 07:52:04 +1000

    Basic dep analysis complete

---
+++ deptools/index.js
@@ -0,0 +1,89 @@
+const babylon = require('babylon');
+const R = require('ramda');
+const fs = require('fs');
+const Promise = require('bluebird');
+const readFile = Promise.promisify(fs.readFile);
+const resolveDependancy = require('resolve');
+const { getDirectoryOfInputFile, getProjectRoot } = require('./path-utils');
+
+function getDependanciesFromFile(fileName) {
+  return new Promise((resolve, reject) => {
+    if (!fileName || (fileName && !fs.existsSync(fileName))) {
+      reject(new Error('Could not find passed file location'));
+      return [];
+    }
+
+    readFile(fileName).then((data) => {
+      const parseResults = babylon.parse(
+        data.toString(),
+        {
+          sourceType: 'module',
+          plugins: [
+            'jsx',
+            'decorators',
+            'classProperties',
+            'classPrivateProperties',
+            'objectRestSpread'
+          ],
+        }
+      );
+
+      const body = parseResults.program.body;
+
+      // resolves with an array of dependancy names from any requires or imports
+      resolve(
+        R.filter(
+          (val) => typeof val === 'string', 
+          R.concat(
+            R.map((val) => val.source.value, R.filter((val) => {
+              return (val.type === 'ImportDeclaration');
+            }, body)),
+            R.map((val) => val.declarations[0].init.arguments[0].value, R.filter((val) => {
+              return ( val.declarations && val.declarations[0].init.callee.name === 'require');
+            }, body))
+          )));
+    }).catch(reject);
+  });
+}
+
+function resolveDependancyToFile(dependancyName) {
+  return new Promise((resolve, reject) => {
+    try {
+      //In the case of relative paths i.e. ./components/component, we need to use the path of the 
+      //input file to resolve it
+      const baseDir = dependancyName.match(/^\.\//g) ? getDirectoryOfInputFile(process.argv[2]) : getProjectRoot(process.cwd());
+      resolveDependancy(
+        dependancyName, 
+        { 
+          basedir: baseDir,
+          extensions: [ '.js' ],
+          moduleDirectory: 'node_modules'
+        }, (err, val) => {
+          if(err) reject(err);
+          else resolve(val);
+        });
+    } catch(e) {
+      reject(e);
+    }
+  });
+}
+
+function resolveMapDependanciesToFile(dependancies) {
+  return R.map(resolveDependancyToFile, dependancies);
+}
+
+async function getDependancies(file) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const dependancies = await getDependanciesFromFile(file);
+
+      Promise.all(resolveMapDependanciesToFile(dependancies)).then(paths => {
+        resolve(paths);
+      }).catch(reject);
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+module.exports = getDependancies;
--- index.js
+++ index.js
@@ -1,98 +1,10 @@
-const babylon = require('babylon');
-const R = require('ramda');
-const fs = require('fs');
-const Promise = require('bluebird');
-const readFile = Promise.promisify(fs.readFile);
-const resolveDependancy = require('resolve');
-const { getDirectoryOfInputFile, getProjectRoot } = require('./path-utils');
-
-function getDependanciesFromFile(fileName) {
-  return new Promise((resolve, reject) => {
-    if (!fileName || (fileName && !fs.existsSync(fileName))) {
-      reject(new Error('Could not find passed file location'));
-      return [];
-    }
-
-    readFile(fileName).then((data) => {
-      const parseResults = babylon.parse(
-        data.toString(),
-        {
-          sourceType: 'module',
-          plugins: [
-            'jsx',
-            'decorators',
-            'classProperties',
-            'classPrivateProperties',
-            'objectRestSpread'
-          ],
-        }
-      );
-
-      const body = parseResults.program.body;
-
-      resolve(
-        R.filter((val) => typeof val === 'string', 
-          R.append(
-            R.map((val) => val.source.value, R.filter((val) => {
-              return (val.type === 'ImportDeclaration');
-            }, body)),
-            R.map((val) => val.declarations[0].init.arguments[0].value, R.filter((val) => {
-              return ( val.declarations && val.declarations[0].init.callee.name === 'require');
-            }, body))
-          )));
-    }).catch(reject);
-
-    return true;
-  });
-}
-
-function resolveDependancyToFile(dependancyName) {
-  return new Promise((resolve, reject) => {
-    try {
-      //In the case of relative paths i.e. ./components/component, we need to use the path of the 
-      //input file to resolve it
-      const baseDir = dependancyName.match(/^\.\//g) ? getDirectoryOfInputFile(process.argv[2]) : getProjectRoot(process.cwd());
-      resolveDependancy(
-        dependancyName, 
-        { 
-          basedir: baseDir,
-          extensions: [ '.js' ],
-          moduleDirectory: 'node_modules'
-        }, (err, val) => {
-          if(err) reject(err);
-          else resolve(val);
-        });
-    } catch(e) {
-      reject(e);
-    }
-  });
-}
-
-function resolveMapDependanciesToFile(dependancies) {
-  return R.map(resolveDependancyToFile, dependancies);
-}
-
-async function start() {
-  const file = process.argv[2];
-  try {
-    const dependancies = await getDependanciesFromFile(file);
-    console.log(dependancies);
-
-    Promise.all(resolveMapDependanciesToFile(dependancies)).then(paths => {
-      console.log(paths);
-    }).catch(
-      (e) => { 
-        console.error(e);
-        console.error('Dependancy was not found in current project space. \
-                     Are you sure you called Sideswipe from the project directory?');
-      }
-    );
-  } catch (e) {
-    console.log(e);
-  }
-    
-  return true;
-}
-
-start();
+const getDependancies = require('./deptools/index');
+getDependancies(process.argv[2]).then((e) => {
+  process.stdout.write(JSON.stringify(e));
+  process.exit(0);
+}).catch((e) => {
+  process.stdout.write(e.message + '\n');
+  process.stdout.write('Dependancy was not found in current project space. Are you sure you called Sideswipe from the project directory?');
+  process.exit(1);
+});
 
